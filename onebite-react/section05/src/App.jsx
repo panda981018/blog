6b24a108a58b7f061d05cdf4a6a48f19bd5b83c2
Jiwon <panda981018@gmail.com>
2024-05-23 15:56:24 +0900

    05/23 한입 크기로 잘라 먹는 리액트 section 4, 5 추가 ~ 5.3) JSX로 UI 표현하기까지

---
+++ onebite-react/section05/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/main";
+import Footer from "./components/Footer";
+
+// 컴포넌트(Component)
+function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
