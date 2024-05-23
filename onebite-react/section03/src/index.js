735a030ca9e802fd8c390a6eb17dff4ba46ab335
Jiwon <panda981018@gmail.com>
2024-05-23 11:34:46 +0900

    05/23 한입 크기로 잘라먹는 리액트 section3 완료

---
+++ onebite-react/section03/src/index.js
@@ -0,0 +1,13 @@
+// CommonJS Module
+// const { add, sub } = require("./math");
+
+// ES Module
+// import mul, { add, sub } from "./math.js";
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
---
+++ onebite-react/section03/src/math.js
@@ -0,0 +1,20 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+export default function multiply(a, b) {
+    return a * b;
+}
+
+// CommonJS Module
+// module.exports = {
+//     add,
+//     sub,
+// };
+
+// ES Module
+// export { add, sub };
