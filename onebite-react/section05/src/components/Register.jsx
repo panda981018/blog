8a50f8b4040c56b1df974ae35e9c563bd36d508b
Jiwon <panda981018@gmail.com>
2024-05-27 16:06:05 +0900

    05/27 한입 크기로 잘라 먹는 리액트 useRef까지 완료

--- onebite-react/section05/src/components/Register.jsx
+++ onebite-react/section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 // 간단한 회원가입 폼
 // 1. 이름
 // 2. 생년월일
@@ -6,53 +6,99 @@ import { useState } from "react";
 // 4. 자기소개
 
 const Register = () => {
-	const [name, setName] = useState("이름");
-	const [birth, setBirth] = useState("");
-	const [country, setCountry] = useState("");
-	const [bio, setBio] = useState("");
+    const [input, setInput] = useState({
+        name: "",
+        birth: "",
+        country: "",
+        bio: "",
+    });
 
-	const onChangeName = (e) => {
-		setName(e.target.value);
-	};
+    const countRef = useRef(0);
+    const inputRef = useRef();
 
-	const onChangeBirth = (e) => {
-		setBirth(e.target.value);
-	};
+    // 통합 이벤트 핸들러
+    const onChange = (e) => {
+        countRef.current++;
+        console.log(countRef.current);
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-	const onChangeCountry = (e) => {
-		setCountry(e.target.value);
-	};
+    const onSubmit = (e) => {
+        if (input.name === "") {
+            // 이름을 입력하는 DOM 요소 포커스
+            inputRef.current.focus();
+        }
+    };
 
-	const onChangeBio = (e) => {
-		setBio(e.target.value);
-	};
+    // const onChangeName = (e) => {
+    //     setInput({
+    //         ...input,
+    //         name: e.target.value,
+    //     });
+    // };
 
-	return (
-		<div>
-			<div>
-				<input
-					type="text"
-					value={name}
-					onChange={onChangeName}
-					placeholder={"이름"}
-				/>
-			</div>
-			<div>
-				<input type="date" value={birth} onChange={onChangeBirth} />
-			</div>
-			<div>
-				<select value={country} onChange={onChangeCountry}>
-					<option>-</option>
-					<option value="kr">한국</option>
-					<option value="us">미국</option>
-					<option value="uk">영국</option>
-				</select>
-			</div>
-			<div>
-				<textarea value={bio} onChange={onChangeBio} />
-			</div>
-		</div>
-	);
+    // const onChangeBirth = (e) => {
+    //     setInput({
+    //         ...input,
+    //         birth: e.target.value,
+    //     });
+    // };
+
+    // const onChangeCountry = (e) => {
+    //     setInput({
+    //         ...input,
+    //         country: e.target.value,
+    //     });
+    // };
+
+    // const onChangeBio = (e) => {
+    //     setInput({
+    //         ...input,
+    //         bio: e.target.value,
+    //     });
+    // };
+
+    return (
+        <div>
+            <div>
+                <input
+                    ref={inputRef}
+                    type="text"
+                    name="name"
+                    value={input.name}
+                    onChange={onChange}
+                    placeholder={"이름"}
+                />
+            </div>
+            <div>
+                <input
+                    type="date"
+                    name="birth"
+                    value={input.birth}
+                    onChange={onChange}
+                />
+            </div>
+            <div>
+                <select
+                    name="country"
+                    value={input.country}
+                    onChange={onChange}
+                >
+                    <option>-</option>
+                    <option value="kr">한국</option>
+                    <option value="us">미국</option>
+                    <option value="uk">영국</option>
+                </select>
+            </div>
+            <div>
+                <textarea name="bio" value={input.bio} onChange={onChange} />
+            </div>
+            <button onClick={onSubmit}>제출</button>
+        </div>
+    );
 };
 
 export default Register;
