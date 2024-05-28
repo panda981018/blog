59e9eacf257cd8b5fe54b75f6b79f6bb8be4b86e
jiwon <panda981018@gmail.com>
2024-05-28 22:34:05 +0900

    05/28 한입 크기로 잘라 먹는 리액트 section08 일부 추가(리스트 렌더링 하기)

--- onebite-react/section08/src/components/List.jsx
+++ onebite-react/section08/src/components/List.jsx
@@ -1,18 +1,41 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
-    return (
-        <div className="List">
-            <h4>Todo List 🌿</h4>
-            <input placeholder="검색어를 입력하세요" />
-            <div className="todos_wrapper">
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
-            </div>
-        </div>
-    );
+const List = ({ todos }) => {
+	const [search, setSearch] = useState("");
+
+	const onChangeSearch = (e) => {
+		setSearch(e.target.value);
+	};
+
+	const getFilteredData = () => {
+		if (search === "") {
+			return todos;
+		}
+
+		return todos.filter((todo) =>
+			todo.content.toLowerCase().includes(search.toLowerCase())
+		);
+	};
+
+	const filteredTodos = getFilteredData();
+
+	return (
+		<div className="List">
+			<h4>Todo List 🌿</h4>
+			<input
+				placeholder="검색어를 입력하세요"
+				value={search}
+				onChange={onChangeSearch}
+			/>
+			<div className="todos_wrapper">
+				{filteredTodos.map((todo) => {
+					return <TodoItem key={todo.id} {...todo} />;
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default List;
--- onebite-react/section08/src/components/TodoItem.jsx
+++ onebite-react/section08/src/components/TodoItem.jsx
@@ -1,14 +1,14 @@
 import "./TodoItem.css";
 
-const TodoItem = () => {
-    return (
-        <div className="TodoItem">
-            <input type="checkbox" />
-            <div className="content">Todo...</div>
-            <div className="date">Date</div>
-            <button>삭제</button>
-        </div>
-    );
+const TodoItem = ({ isDone, content, date }) => {
+	return (
+		<div className="TodoItem">
+			<input checked={isDone} type="checkbox" readOnly />
+			<div className="content">{content}</div>
+			<div className="date">{new Date(date).toLocaleDateString()}</div>
+			<button>삭제</button>
+		</div>
+	);
 };
 
 export default TodoItem;
