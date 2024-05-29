26be0810d2c36a07615e4f45d2442921c6ea6072
Jiwon <panda981018@gmail.com>
2024-05-29 09:37:43 +0900

    05/29 한입 크기로 잘라 먹는 리액트 section08 완료

--- onebite-react/section08/src/components/TodoItem.jsx
+++ onebite-react/section08/src/components/TodoItem.jsx
@@ -1,14 +1,26 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ isDone, content, date }) => {
-	return (
-		<div className="TodoItem">
-			<input checked={isDone} type="checkbox" readOnly />
-			<div className="content">{content}</div>
-			<div className="date">{new Date(date).toLocaleDateString()}</div>
-			<button>삭제</button>
-		</div>
-	);
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    };
+
+    const onClickDeleteButton = () => {
+        onDelete(id);
+    };
+
+    return (
+        <div className="TodoItem">
+            <input
+                checked={isDone}
+                type="checkbox"
+                onChange={onChangeCheckbox}
+            />
+            <div className="content">{content}</div>
+            <div className="date">{new Date(date).toLocaleDateString()}</div>
+            <button onClick={onClickDeleteButton}>삭제</button>
+        </div>
+    );
 };
 
 export default TodoItem;
