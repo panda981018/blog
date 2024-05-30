da1e0c67d6f9aae1748ed4fa01d80849277ec6da
Jiwon <panda981018@gmail.com>
2024-05-30 16:01:03 +0900

    05/30 한입 크기로 잘라 먹는 리액트 section12 일부 추가

---
+++ onebite-react/section12/src/App.jsx
@@ -0,0 +1,54 @@
+import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import NotFound from "./pages/Notfound";
+import Button from "./components/Button";
+import getEmotionImage from "./util/get-emotion-image";
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+function App() {
+    const nav = useNavigate();
+
+    const onClickButton = () => {
+        nav("/new");
+    };
+
+    return (
+        <>
+            <Button
+                text={"123"}
+                type={"DEFAULT"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭");
+                }}
+            />
+            <Button
+                text={"123"}
+                type={"POSITIVE"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭");
+                }}
+            />
+            <Button
+                text={"123"}
+                type={"NEGATIVE"}
+                onClick={() => {
+                    console.log("123번 버튼 클릭");
+                }}
+            />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/diary/:id" element={<Diary />} />
+                <Route path="/new" element={<New />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
+    );
+}
+
+export default App;
