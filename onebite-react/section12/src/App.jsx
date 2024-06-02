dfdfd50fb61a81ada5400215291c9b0f8e92969e
jiwon <panda981018@gmail.com>
2024-06-02 11:19:16 +0900

    06/01 한입 크기로 잘라 먹는 리액트 section12 일부 추가(Home 페이지 구현 완료)

--- onebite-react/section12/src/App.jsx
+++ onebite-react/section12/src/App.jsx
@@ -10,15 +10,21 @@ import Edit from "./pages/Edit";
 const mockData = [
 	{
 		id: 1,
-		createdDate: new Date().getTime(),
+		createdDate: new Date("2024-06-02").getTime(),
 		emotionId: 1,
 		content: "1번 일기 내용",
 	},
 	{
 		id: 2,
-		createdDate: new Date().getTime(),
+		createdDate: new Date("2024-06-01").getTime(),
 		emotionId: 2,
 		content: "2번 일기 내용",
+	},
+	{
+		id: 3,
+		createdDate: new Date("2024-05-12").getTime(),
+		emotionId: 3,
+		content: "3번 일기 내용",
 	},
 ];
 
@@ -39,12 +45,12 @@ function reducer(state, action) {
 	}
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
 	const [data, dispatch] = useReducer(reducer, mockData);
-	const idRef = useRef(3);
+	const idRef = useRef(4);
 
 	// 새로운 일기 추가
 	const onCreate = (createdDate, emotionId, content) => {
@@ -82,25 +88,6 @@ function App() {
 
 	return (
 		<>
-			<button
-				onClick={() => {
-					onCreate(new Date().getTime(), 1, "hello");
-				}}>
-				일기 추가 테스트
-			</button>
-			<button
-				onClick={() => {
-					onUpdate(2, new Date().getTime(), 3, "수정된 일기입니다");
-				}}>
-				일기 수정 테스트
-			</button>
-			<button
-				onClick={() => {
-					onDelete(1);
-				}}>
-				일기 삭제 테스트
-			</button>
-
 			<DiaryStateContext.Provider value={data}>
 				<DiaryDispatchContext.Provider
 					value={{
