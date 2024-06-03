214289dd098c1bf3e924deb917f5dd34149aa399
Jiwon <panda981018@gmail.com>
2024-06-03 11:04:10 +0900

    06/03 한입 크기로 잘라 먹는 리액트 section12 일부 추가(New 페이지 구현 완료)

--- onebite-react/section12/src/util/get-emotion-image.js
+++ onebite-react/section12/src/util/get-emotion-image.js
@@ -5,20 +5,18 @@ import emotion4 from "./../assets/emotion4.png";
 import emotion5 from "./../assets/emotion5.png";
 
 export function getEmotionImage(emotionId) {
-	switch (emotionId) {
-		case 1:
-			return emotion1;
-		case 2:
-			return emotion2;
-		case 3:
-			return emotion3;
-		case 4:
-			return emotion4;
-		case 5:
-			return emotion5;
-		default:
-			return null;
-	}
+    switch (emotionId) {
+        case 1:
+            return emotion1;
+        case 2:
+            return emotion2;
+        case 3:
+            return emotion3;
+        case 4:
+            return emotion4;
+        case 5:
+            return emotion5;
+        default:
+            return null;
+    }
 }
-
-// export default getEmotionImage;
